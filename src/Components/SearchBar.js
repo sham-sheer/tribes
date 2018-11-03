1646a491ca9cd8136477f35daf2404ae52337c42
zenghou <zenghoulim@outlook.com>
2018-11-03 02:46:31 -0700

    Refactor SearchBar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,53 +1,47 @@
 import React from 'react';
 
-
 export default class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchString: '',
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({searchString: event.target.value});
+  handleSearchInputKeyPress = (event) => {
+    this.setState({
+      searchString: event.target.value
+    });
   }
 
-  handleSubmit(event) {
+  handleSearchButtonClicked = (event) => {
     event.preventDefault();
     this.props.handleFilter(this.state.searchString);
   }
 
   render() {
     return (
-      <div>
-      <nav className="level">
-        <div className="level-left">
-          <div className="level-item">
-          <form onSubmit={this.handleSubmit} className="form-inline">
-           <div className="form-group mb-2">
+      <div className="input-search-container">
+        <nav className="navigation-component">
+          <form onSubmit={this.handleSearchButtonClicked} className="form-inline">
+            <div className="search-input-box mb-2">
               <input
                 name="Issue"
                 type="text"
                 className="form-control"
-                placeholder="Search Events here"
+                placeholder="Enter event here"
                 value={this.state.searchString}
-                onChange={this.handleChange} />
+                onChange={this.handleSearchInputKeyPress}/>
             </div>
-           <div>
-           <input
-             type="submit"
-             value="Search"
-             className="btn btn-outline-dark col-sm form-group mb-2" />
+            <div className="search-button">
+             <input
+               type="submit"
+               value="Search"
+               className="btn btn-outline-dark col-sm form-group mb-2"/>
             </div>
-        </form>
-          </div>
-        </div>
-      </nav>
-
-    </div>
+          </form>
+        </nav>
+      </div>
     );
   }
 }
