8d03c5baa9eb1953c627d7e90fbdd69f4de982ec
zenghou <zenghoulim@outlook.com>
2018-11-03 03:14:15 -0700

    Refactor EventsContainer

--- src/Containers/EventsContainer.js
+++ src/Containers/EventsContainer.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import axios from 'axios';
-import List from '../Components/List.js';
+import EventCardContainer from '../Components/EventCardContainer.js';
 import SearchBar from '../Components/SearchBar.js';
 import pic from './download.png';
 
-
+const DEFAULT_SEARCH_QUERY = 'HACK_BELGIUM';
+const EVENT_BRITE_SECURITY_TOKEN = 'GUOC2RZKKGQHHFMHXLON';
 
 class EventsContainer extends React.Component {
   state = {
     events: [],
     activePage: 1,
-    searchQuery: 'HACK_BELGIUM'
+    searchQuery: DEFAULT_SEARCH_QUERY
   }
 
   componentDidMount() {
@@ -25,30 +26,38 @@ class EventsContainer extends React.Component {
   }
 
   fetchEvents = () => {
-    axios.get(`https://www.eventbriteapi.com/v3/events/search/?token=GUOC2RZKKGQHHFMHXLON&q=${this.state.searchQuery}`)
-    .then(resp => {
-      console.log(resp)
+    axios.get(`https://www.eventbriteapi.com/v3/events/search/?token=` + EVENT_BRITE_SECURITY_TOKEN + `&q=${this.state.searchQuery}`)
+    .then(response => {
+      console.log(response);
       this.setState({
-        events: resp.data.events,
+        events: response.data.events,
       });
     })
     .catch(error => {
-      console.log(error)
+      console.log(error);
     })
   }
 
   render() {
-    let styleobj = { fontSize: 34, textAlign : 'left', marginRight: 670, color: "#067655" };
+    let styleObj = { fontSize: 34, textAlign : 'left', marginRight: 670, color: "#067655" };
     let styleImg = { height: 50, width: 50 };
+
     return(
       <div className="container">
+
+        {/*Navigation Container*/}
         <nav className="navbar navbar-light bg-light">
-        <img src={pic} style={styleImg}></img>
-          <h1 style={styleobj}>tribes</h1>
-          <SearchBar handleFilter={this.filterResults} />
+          <img src={pic} style={styleImg}></img>
+            <h1 style={styleObj}>tribes</h1>
+            <SearchBar handleFilter={this.filterResults}/>
         </nav>
-        <div><List data={this.state.events} /></div>
-      </div>);
+
+        {/*Event Cards Container*/}
+        <div>
+          <EventCardContainer events={this.state.events}/>
+        </div>
+      </div>
+    );
   }
 }
 
